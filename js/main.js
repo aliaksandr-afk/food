b07178fc372821b234bf8ee2919ef38499f46e62
ALeks Baranov <aleks.feedback@gmail.com>
2022-06-28 23:33:31 +0300

    create proj

---
+++ js/main.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(2);
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
+
+    //timer
+
+
+    const deadLine = '2020-05-11';
+
+    function getTimeRemainig(endtime) {
+        
+    }
+
+    const now = new Date('2020, 05, 01')
+    console.log(now.setHours(18))
+    console.log(now)
+})
+
